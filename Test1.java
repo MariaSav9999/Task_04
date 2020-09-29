f7c6a18470add9b5a396bad197db4150446814ac
MariaSav9999 <68650418+MariaSav9999@users.noreply.github.com>
2020-09-29 15:01:38 +0300

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -1,5 +1,14 @@
 package by.htp.les07.main;
 
 public class Test1 {
+	 public int a, b;
+	 public int max() {
+		 if (a > b)
+		      return a;
+		    return b;
+		  }
+		 public int sum() {
+		    return a+b;
+		  }
+	}
 
-}
