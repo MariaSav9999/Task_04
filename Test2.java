b0068630e51d6c5cad53eb05671bf529aaa18836
MariaSav9999 <68650418+MariaSav9999@users.noreply.github.com>
2020-09-29 15:02:05 +0300

    Update Test2.java

--- Test2.java
+++ Test2.java
@@ -1,5 +1,25 @@
 package by.htp.les07.main;
 
 public class Test2 {
-
+	    private int a, b;
+	    public Test2() {
+	      a = 3; 
+	      b = 7;
+	    }
+	    public Test2(int a_, int b_) {
+	      a = a_;
+	      b = b_;
+	    }
+		public int getA() {
+			return a;
+		}
+		public void setA(int a) {
+			this.a = a;
+		}
+		public int getB() {
+			return b;
+		}
+		public void setB(int b) {
+			this.b = b;
+		}
 }
